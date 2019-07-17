fb3fc61314a71cbdb768a17ed4f30bfc00fe89d9
Daniel Cortes <d5t5c5@yahoo.com>
2019-07-16 21:34:11 -0500

    feat: Made routefolder and api for user

--- models/User.js
+++ models/User.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const UserSchema = new mongoose.Schema({
   username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
     type: String,
     required: true,
     unique: true
