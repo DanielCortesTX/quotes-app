702bd41857ff6941682492d3ad8b1e792bfd741d
Daniel Cortes <d5t5c5@yahoo.com>
2019-07-17 07:54:15 -0500

    Fix: Register user now returns jwtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const keys = require('../../config/keys')
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../../models/User')
@@ -55,7 +56,7 @@ router.post('/', [
 
     jwt.sign(
       payload,
-      config.get('secret'),
+      keys.secret,
       { expiresIn: 360000},
       (err, token) => {
         if(err) throw err
