22a23a1a472fe866fcb4c89d51aebb5bc8dc33fc
Daniel Cortes <d5t5c5@yahoo.com>
2019-08-06 20:02:38 -0500

    feat: Removed the token deletion from setAuthtoken

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -4,7 +4,8 @@ const setAuthToken = token => {
   if(token) {
     axios.defaults.headers.common['x-auth-token'] = token
   } else {
-    delete axios.defaults.headers.common['x-auth-token']
+    // delete axios.defaults.headers.common['x-auth-token']
+    console.log('Token issue')
   }
 }
 
