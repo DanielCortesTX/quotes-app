c59d574cd36c68de9d6091533177c0b5a7b7af6e
Daniel Cortes <d5t5c5@yahoo.com>
2019-08-13 17:58:29 -0500

    feat: Removed login and register from logged nabar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -32,12 +32,6 @@ class Navbar extends Component {
           <Link className="nav-link" to="/add">Add</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">Register</Link>
-        </li>
-        <li className="nav-item">
           <Link className="nav-link" to="/userpage">User page</Link>
         </li>
         <li>
