eaf1b653c9e8921dd7e3354c20750c705e55ad7a
unknown <dcorteswebdev@gmail.com>
2019-09-29 17:07:51 -0500

    test from new laptop

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Quote me on it!</h1>
+      <h1>Quote me on it!!!!</h1>
       <p>Welcome to "Quote me on it!" And app built to help you ot down meaningfull expressions and such.</p>
     </div>
   )
