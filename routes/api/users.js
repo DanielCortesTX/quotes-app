8a7c84a7f7057462380bfae2a66e452ae48e0fab
unknown <dcorteswebdev@gmail.com>
2019-12-05 16:46:38 -0600

    feat: Added Logout route

--- routes/api/users.js
+++ routes/api/users.js
@@ -58,6 +58,23 @@ router.get('/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
+// @route POST api/users/logout
+// @desc Logout
+// @access Private
+router.post('/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+
+    res.send()
+  } catch (e) {
+    console.log(e)
+    res.status(500).send()
+  }
+})
+
 module.exports = router
 
 
