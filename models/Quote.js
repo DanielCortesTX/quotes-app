4a2e5452a7f90f439f6caec815be25ec47481366
unknown <dcorteswebdev@gmail.com>
2019-12-06 10:40:17 -0600

    feat: Updated Quote Schema
    - Changed date to timestamp
    - Changed user refence to owner
    - Attempt with new to make quotes with new Schema

--- models/Quote.js
+++ models/Quote.js
@@ -1,38 +1,34 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// const Schema = mongoose.Schema
 
 const QuoteSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user'
+  },
   author: {
     type: String,
     required: true,
     unique: true
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  },
   text: {
     type: String,
     required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
   },
   dateOfQuote: {
     type: String
   },
   bodyOfWork: {
     type: String
-  },
-  likes: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      }
-    }
-  ]
+  }
+}, {
+  timestamps: true
 })
 
 module.exports = Quote = mongoose.model('quote', QuoteSchema)
+
+// user: {
+//   type: Schema.Types.ObjectId,
+//   ref: 'users'
+// },
