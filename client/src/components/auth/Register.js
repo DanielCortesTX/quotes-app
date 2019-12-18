a1bade15b6fc5871040d1d4dbef26da768e97484
unknown <dcorteswebdev@gmail.com>
2019-12-18 12:24:34 -0600

    feat: Moved routes to new auth folder

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { register } from '../actions/auth'
+import { register } from '../../actions/auth'
 
 const Register = ({register, isAuthenticated}) => {
   const [formData, setFormData] = useState({
