2b4466efea07ffcd2e17a405c07bd9779b3398fc
unknown <dcorteswebdev@gmail.com>
2020-01-14 14:50:57 -0600

    feat: Added search form layout

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+  const [formData, setFormData] = useState({
+    filter: 'author',
+    search: ''
+  })
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+  const onSubmit = async e => {
+    e.preventDefault()
+    console.log(formData)
+  }  
   return (
     <div>
       THis is the search page.
+      <div className="card">
+        <form onSubmit={e => onSubmit(e)}>
+          <select onChange={e => onChange(e)}>
+            <option>Author</option>
+            <option>Year</option>
+            <option>Body of Work</option>
+          </select>
+          <input 
+            type="text"
+            name="search"
+            placeholder="Search for"
+            onChange={e => onChange(e)}/>
+          <input type="submit"/>
+        </form>
+      </div>
     </div>
   )
 }
