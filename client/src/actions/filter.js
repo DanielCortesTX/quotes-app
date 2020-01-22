af7fa99c2e47819607f221f6f2a87e47dc93c083
unknown <dcorteswebdev@gmail.com>
2020-01-22 14:55:25 -0600

    chore: Removed unused definitions

--- client/src/actions/filter.js
+++ client/src/actions/filter.js
@@ -1,6 +1,4 @@
-import axios from 'axios'
-import { getFilteredQuotes } from './quotes'
-import { SET_FILTERS, GET_QUOTES } from './types'
+import { SET_FILTERS } from './types'
 
 export const setFilters = (formData) => async dispatch => {
   try {
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,5 +1,4 @@
-import React, { Component, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import QuoteDisplay from './QuoteDisplay'
