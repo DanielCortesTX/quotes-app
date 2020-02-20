6f4033378de39a590bb6de73b8d227048378b66c
unknown <dcorteswebdev@gmail.com>
2020-02-20 14:47:29 -0600

    feat: Added back button to Display Page

--- client/src/screens/Display.js
+++ client/src/screens/Display.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 // import PropTypes from 'prop-types'
 import Loading from '../components/frequents/Loading'
+import { Link } from 'react-router-dom'
 import { setActiveQuote } from '../actions/quotes'
 
 const QuoteDisplay = ({match, setActiveQuote, quote, loading}) => {
@@ -15,13 +16,14 @@ const QuoteDisplay = ({match, setActiveQuote, quote, loading}) => {
   if(!loading || quote === undefined){
     display = <Loading />
   } else {
-    display = <div className="page p-1">
-      <div className="quote-specifics">
+    display = <div className="page p-1 quote-specifics">
+      <div className="">
         <h2>"{quote.text}"</h2>
         <p>-{quote.author}</p>
         <p>{quote.dateOfQuote}</p>
         <p>{quote.bodyOfWork}</p>
       </div>
+      <Link to="/userpage" className="btn my-1">Back</Link>
     </div>
   }
   
